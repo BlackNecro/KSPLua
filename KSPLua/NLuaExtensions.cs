0a9457840e1ce0db379b9fd6f12140669039089c
BlackNecro <nick.noether@gmail.com>
2014-08-10 02:06:31 +0200

    Removed automatic native lib installing as of now

---
+++ KSPLua/NLuaExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NLua;
+
+
+namespace KSPLua
+{
+    public static class NLuaExtensions
+    {
+        public static LuaTable CreateTable(this Lua luaInst)
+        {
+            return (LuaTable) luaInst.DoString("return {}")[0];
+        }
+    }
+}
